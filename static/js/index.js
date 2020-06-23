6407cc5fe3f947fe7406ff904feee732a3125266
ValentineSean <seanchanengeta@gmail.com>
2020-06-23 07:35:25 +0200

    Techlink initial templates

---
+++ static/js/index.js
@@ -0,0 +1,19 @@
+// LOG IN SECTION
+function displayLogin(){
+    document.getElementById("login").style.display = "block";
+    document.getElementById("signup").style.display = "none";
+}
+
+function hideLogin(){
+    document.getElementById("login").style.display = "none"
+}
+
+// SIGN UP SECTION
+function displaySignup(){
+    document.getElementById("signup").style.display = "block";
+    document.getElementById("login").style.display = "none";
+}
+
+function hideSignup(){
+    document.getElementById("signup").style.display = "none";
+}
